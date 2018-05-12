fcf23402e6475e1eff688046924974225bf45260
Alex Abramov <37221107+alexabramov2610@users.noreply.github.com>
2018-05-12 22:06:11 +0300

    message class done

--- src/Message.java
+++ src/Message.java
@@ -1,8 +1,31 @@
 
-public class Message {
-
-	public Message() {
-		// TODO Auto-generated constructor stub
+public class Message 
+{
+	private String _receiver;
+	private String _sender;
+	private String _content;
+	
+	/**
+	 * this constructor initializes a Message object with given strings
+	 * @param sender is the person who sends the message
+	 * @param receiver is the person who gets the message
+	 * @param content is the message text delivered by the sender to the receiver
+	 */
+	public Message(String sender, String receiver, String content) 
+	{
+		_receiver = receiver;
+		_sender = sender;
+		_content = content;
 	}
-
+	
+	/**
+	 * returns a string which includes the message's info:
+	 * sender, receiver and message content
+	 */
+	public String toString()
+	{
+		return "From:" + _sender + '\n' +
+							"To:" + _receiver + '\n' +
+							_content;
+	}
 }
--- src/Messages.java
+++ src/Messages.java
@@ -1,8 +1,113 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Messages {
+public class Messages 
+{
+	private Message[] _list;
+	private int size = 0;
+	private String _fileName; 		// file name
+	private FileReader _fileReader;
+	private BufferedReader _bufferedReader;
 
-	public Messages() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * initializes the Messages list creation
+	 * @param fileName
+	 */
+	public void generateMessages(String fileName)
+	{
+		_fileName = fileName;
+		try {
+			_fileReader = new FileReader(_fileName);
+			_bufferedReader = new BufferedReader(_fileReader);
+			getListSize();
+			_list = new Message[size];
+			buildArray();
+			_bufferedReader.close();
+		} 
+		catch(IOException ex) {
+			System.out.println("error: unable to open file '" + _fileName + "'");
+		}		
+	}
+	/**
+	 * adds the Messages to the list
+	 */
+	private void buildArray() {
+		if (_list.length == 1)
+		{
+			System.out.println("no messages found in '" + _fileName + "'");
+			return;
+		}
+		String currentMessage;
+		int msgNumber = 0;
+		try {
+
+			currentMessage = _bufferedReader.readLine();
+			while(msgNumber < size && currentMessage != null) 
+			{
+				String sender = _bufferedReader.readLine().substring(5);
+				String receiver = _bufferedReader.readLine().substring(3);
+				String message = _bufferedReader.readLine();
+				while ((currentMessage = _bufferedReader.readLine()) != null && !currentMessage.equals("#"))
+					message += '\n' + currentMessage;
+				_list [msgNumber] = new Message (sender,receiver,message);
+				msgNumber++;
+			}   
+			_bufferedReader.close();  
+		}
+		catch(IOException ex) {
+			System.out.println("error: unable to read file '"  + _fileName + "'");   
+		}
+	}
+	/**
+	 * counts the times '#' appears in the txt file
+	 * @return number '#' is written
+	 */
+	private void getListSize() throws IOException {
+		String currentMessage;
+		int counter = 0;
+		try 
+		{
+			while((currentMessage = _bufferedReader.readLine()) != null) 
+				if (currentMessage.equals("#"))
+					counter++;
+		}
+		catch(IOException ex) 
+		{
+			System.out.println("error: unable to read file '"  + _fileName + "'");   
+		}
+		if (counter > 0)
+			size = counter;
+		// we have popped all lines in the file, so we must reopen it
+		_fileReader = new FileReader(_fileName);
+		_bufferedReader = new BufferedReader(_fileReader);
+	}
+
+	
+	/**
+	 *  prints all messages in the given order, including '#'
+	 */
+	public String toString()
+	{
+		if (size == 0)
+			return "error: no messages found";
+		String ans = "";
+		for (int i=0; i<size;i++)
+		{
+			if (i != size-1)
+				ans += _list[i].toString() + '\n' + "#"  + '\n';
+			else
+				ans += _list[i].toString();
+		}
+		return ans;
+	}
+	public static void main(String args[])
+	{
+//		Messages msgs = new Messages();
+		Messages messages = new Messages();
+		messages.generateMessages(System.getProperty("user.dir")+"/messages.txt");
+		String ans = messages.toString();
+		System.out.println(ans);
 	}
 
 }
