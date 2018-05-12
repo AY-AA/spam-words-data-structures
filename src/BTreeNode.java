c5ae0b86bf78d89e5d975ea04c292f98d08bd6a1
Alex Abramov <37221107+alexabramov2610@users.noreply.github.com>
2018-05-12 17:20:44 +0300

    d

--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -1,8 +1,16 @@
 
-public class BTreeNode {
+public class BTreeNode 
+{
 
-	public BTreeNode() {
-		// TODO Auto-generated constructor stub
+	private int _n;			//number of keys in node
+	private boolean _leaf;  //true if node is a leaf
+	private int _t;			//minimum degree
+	
+	
+	public BTreeNode(int t, boolean leaf) 
+	{
+		_t = t;
+		_leaf = leaf;
 	}
 
 }
